2b549b9b54abee7816456c9b70f01f98babe7cc6
Rodrigo Saraya <linkin_rodx@hotmail.com>
2020-03-18 02:59:59 -0500

    add components add angular-material

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StockComponent } from './components/stock/stock.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { DishListComponent } from './components/dish-list/dish-list.component';
+import { DishComponent } from './components/dish/dish.component';
+import { ProductComponent } from './components/product/product.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockComponent,
+    LoginComponent,
+    ProductListComponent,
+    DishListComponent,
+    DishComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
